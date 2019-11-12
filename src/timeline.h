07455ba731040cdd99a935029bed95a2ba41a0e5
Leo <42943640+yymmaa0000@users.noreply.github.com>
2019-11-12 10:21:38 -0500

    Revert "Revert "Merge pull request #7 from cardadfar/master""
    This reverts commit 1aff103d4f61719e5bce02da693ef7b47f2e704e.

--- src/timeline.h
+++ src/timeline.h
@@ -142,7 +142,8 @@ class Timeline {
   bool isLooping;
 
   // this stores which button the user is currently hovering over.
-  int hover_button_index;
+  int hover_button_index = -1;
+  int selected_button_index = -1;
 
   // WARNING: Don't change the values REWIND - LOOP.
   enum e_Element {
